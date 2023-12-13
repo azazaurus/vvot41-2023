a2b9db553b8d821acc59f7162effc2daa858294f
azazaurus <1maxovmax!@gmail.com>
2023-12-13 22:31:07 +0400

    implemented error reporting

--- cloudphoto/src/main/java/cloudphoto/Application.java
+++ cloudphoto/src/main/java/cloudphoto/Application.java
@@ -1,17 +1,31 @@
 package cloudphoto;
 
+import cloudphoto.common.result.*;
 import cloudphoto.configuration.*;
 
 public class Application {
+	private static final int errorExitCode = 1;
+
 	public Application() {
 	}
 
-	public void run(String[] args) {
+	public Result run(String[] args) {
+		return Result.success;
 	}
 
 	public static void main(String[] args) {
-		var diContainer = ApplicationConfigurator.createDiContainer();
-		var application = diContainer.getBean(Application.class);
-		application.run(args);
+		try {
+			var diContainer = ApplicationConfigurator.createDiContainer();
+			var application = diContainer.getBean(Application.class);
+
+			var applicationRunResult = application.run(args);
+			if (applicationRunResult.isFailure())
+				System.exit(errorExitCode);
+		}
+		catch (Throwable throwable) {
+			System.err.print("Unexpected error: ");
+			throwable.printStackTrace(System.err);
+			System.exit(errorExitCode);
+		}
 	}
 }
